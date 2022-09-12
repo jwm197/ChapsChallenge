b92b2125aa5931b988da88deb13c0a2a4e359429
Jia Wei Leong <leongjia1@myvuw.ac.nz>
2022-09-12 18:31:28 +1200

    Initial commit for persistency

--- src/nz/ac/vuw/ecs/swen225/gp22/Persistency/Persistency.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Persistency/Persistency.java
@@ -4,5 +4,16 @@ package nz.ac.vuw.ecs.swen225.gp22.Persistency;
  * Persistency module
  */
 
+class Persistency{
+    public boolean loadXML(){
+        return false;
+     } 
+     
+     public boolean saveXML(){
+        return false;
+     }
+  }
+ 
+
 
  
