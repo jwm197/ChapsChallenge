5f788e9249d90ba4cd022688085fa1282859a4b4
Jia Wei Leong <leongjia1@myvuw.ac.nz>
2022-09-12 19:54:03 +1200

    Initial construction for loadXML

--- src/nz/ac/vuw/ecs/swen225/gp22/Persistency/Persistency.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Persistency/Persistency.java
@@ -1,11 +1,24 @@
 package nz.ac.vuw.ecs.swen225.gp22.Persistency;
 
+import java.io.*;
+
 /**
  * Persistency module
  */
 
 class Persistency{
-    public boolean loadXML(){
+    public boolean loadXML() throws IOException {
+        try{
+            File xmlFile = new File("/level1.xml");
+            if (!xmlFile.exists()){
+                System.out.println("XML file doesn't exist");
+                return false;
+            }
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+
         return false;
      } 
      
