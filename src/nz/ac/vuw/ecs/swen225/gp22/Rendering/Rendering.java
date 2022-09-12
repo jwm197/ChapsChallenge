d3a0b697083e45943ce6eca1786286d36b2e63a1
Anfri <haywarmatt@ecs.vuw.ac.nz>
2022-09-12 14:18:05 +1200

    initial commit for branch 'render'

--- src/nz/ac/vuw/ecs/swen225/gp22/Rendering/Rendering.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Rendering/Rendering.java
@@ -1,5 +1,21 @@
 package nz.ac.vuw.ecs.swen225.gp22.Rendering;
 
+import javax.swing.JLayeredPane;
+
 /**
  * Rendering module
  */
+
+public class Rendering {
+	public static void main(String... args) {
+		System.out.println("hello world");
+		
+		GamePanel p = new GamePanel();
+	}
+}
+
+class GamePanel extends JLayeredPane {
+	private static final long serialVersionUID = 6329522230801811716L;
+	
+	
+}
