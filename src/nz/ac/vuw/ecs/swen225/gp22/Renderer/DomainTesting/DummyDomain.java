4a02482a2fccd40e4bc1c8e844f80217a208d9bb
Anfri <haywarmatt@ecs.vuw.ac.nz>
2022-09-19 00:18:29 +1200

    Update test entity to use Position

--- src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/DummyDomain.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/DummyDomain.java
@@ -1,19 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp22.Renderer.DomainTesting;
 
-import nz.ac.vuw.ecs.swen225.gp22.Renderer.TextureHandling.TextureSequence;
-
-interface Animator {
-	void queueMovingImgSequence(Entity entity, TextureSequence images, Position initialPos, Position finalPos, int tickLength, int frameTicks, Runnable onCompletion);
+record Level(Map m, DummyPlayer dummy, Animator animator) {
+	
 }
 
-class Level {
-	Map m;
-	DummyPlayer dummy;
-	
-	Level() {
-		m = new Map(12, 12);
-		dummy = new DummyPlayer(0,0);
+class DummyDomain {
+	public static void main(String... args) {
+		Level l = new Level(new Map(12,12), new DummyPlayer(new Position(0,0)), Animator.NONE);
 	}
-	
-	
 }
--- src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/Entity.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/Entity.java
@@ -3,14 +3,23 @@ package nz.ac.vuw.ecs.swen225.gp22.Renderer.DomainTesting;
 import nz.ac.vuw.ecs.swen225.gp22.Renderer.TextureHandling.Texture;
 import nz.ac.vuw.ecs.swen225.gp22.Renderer.TextureHandling.Textures;
 
-abstract class Entity {
-	int x, y;
+interface Entity {
+	Position getPos();
 }
 
-class DummyPlayer extends Entity {
+class DummyPlayer implements Entity {
 	public static Texture test = Textures.Scrungle;
-	DummyPlayer(int x, int y) {
-		this.x = x;
-		this.y = y;
+	private Position pos;
+	DummyPlayer(Position pos) {
+		this.pos = pos;
+	}
+	public Position getPos() {
+		return pos;
+	}
+	public void moveLeft(Animator e) {
+		Position newPos = new Position(pos.x(), pos.y());
+		e.AnimateMoving(null, test, newPos, 60, () -> {
+			pos = newPos;
+		});
 	}
 }
