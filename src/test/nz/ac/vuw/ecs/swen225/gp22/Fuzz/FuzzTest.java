5a72fc5d3c1562951cbe87665ec0a4c084b7fdee
Hanning Qu <quhann@ecs.vuw.ac.nz>
2022-09-19 14:28:38 +1200

    Replace FuzzTest.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/Fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/Fuzz/FuzzTest.java
@@ -1,5 +1,15 @@
-package test.nz.ac.vuw.ecs.swen225.gp22.Fuzz;
+package nz.ac.vuw.ecs.swen225.gp22.Fuzz;
 
 /**
- * Fuzz module
+ * Fuzz module AMONGUS
  */
+class FuzzTest{
+    //plays level 1
+    void test1(){
+
+    }
+    //plays level 2
+    void test2(){
+
+    }
+}
