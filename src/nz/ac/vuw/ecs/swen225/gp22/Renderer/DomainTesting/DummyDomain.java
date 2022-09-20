179b466ed9bd4018afb436e1dc53363a9f429463
Anfri <haywarmatt@ecs.vuw.ac.nz>
2022-09-21 01:44:44 +1200

    Update DummyDomain test class and add Linux optimisations

--- src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/DummyDomain.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/DummyDomain.java
@@ -15,14 +15,14 @@ class DummyDomain extends JFrame {
 	RenderPanel debugRenderer;
 	
 	DummyDomain() {
-		debugLevel = new Level(new Map(12,12), new DummyPlayer(new Position(0,0)), Animator.NONE);
+		debugLevel = new Level(new Map(12,12), new DummyPlayer(new Position<Integer>(0,0)), Animator.NONE);
 		debugRenderer = new RenderPanel(debugLevel);
 		
 		setSize(400,400);
 		add(debugRenderer);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		Timer timer = new Timer(1000, e -> tick());
+		Timer timer = new Timer(17, e -> tick());
 		
 		addWindowListener(new WindowAdapter() {
 			public void windowClosed(WindowEvent e) {
@@ -35,10 +35,12 @@ class DummyDomain extends JFrame {
 	
 	public void tick() {
 		debugLevel.tick();
+		debugRenderer.tick();
 		debugRenderer.repaint();
 	}
 	
 	public static void main(String... args) {
+		System.setProperty("sun.java2d.opengl", "true");
 		SwingUtilities.invokeLater(DummyDomain::new);
 	}
 }
