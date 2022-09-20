d4e5dfa0a6985e43f96f488f338e60e05452a700
Anfri <haywarmatt@ecs.vuw.ac.nz>
2022-09-21 01:27:16 +1200

    Fix bug with tweening

--- src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/Position.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/Position.java
@@ -4,12 +4,16 @@ public record Position<T extends Number>(T x, T y) {
 	
 	public static Position<Double> tween(Position<?> a, Position<?> b, double tween) {
 		return new Position<Double>(
-			a.x().doubleValue()*tween + b.x().doubleValue()*(1-tween),
-			a.y().doubleValue()*tween + b.y().doubleValue()*(1-tween)
+			a.x().doubleValue()*(1-tween) + b.x().doubleValue()*tween,
+			a.y().doubleValue()*(1-tween) + b.y().doubleValue()*tween
 		);
 	}
 	
 	public Position<Double> doubleValue() {
 		return new Position<Double>(this.x().doubleValue(), this.y().doubleValue());
+	}
+
+	public Position<Integer> intValue() {
+		return new Position<Integer>((int)Math.round(this.x().doubleValue()), (int)Math.round(this.y().doubleValue()));
 	}
 }
