9e1d30b3d73ee20a651c7792f9a11fae0c3b7119
Hanning Qu <quhann@ecs.vuw.ac.nz>
2022-09-23 10:57:32 +1200

    test 1 which invokes methods in chapschallenge.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/Fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/Fuzz/FuzzTest.java
@@ -1,15 +1,51 @@
-package nz.ac.vuw.ecs.swen225.gp22.Fuzz;
+package test.nz.ac.vuw.ecs.swen225.gp22.Fuzz;
+import nz.ac.vuw.ecs.swen225.gp22.App.*;
+import static org.junit.Assert.fail;
+import org.junit.Test;
 
+import java.awt.Event;
+import java.awt.event.KeyEvent;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
 /**
- * Fuzz module AMONGUS
+ * Fuzz module 
  */
-class FuzzTest{
+public class FuzzTest{
     //plays level 1
-    void test1(){
-
+    @Test
+    public void test1(){
+        try {
+            SwingUtilities.invokeLater(ChapsChallenge::new);
+            //ChapsChallenge g = new ChapsChallenge();
+            /*g.LoadGame();
+            g.GameHelp();
+            g.GameScreen();
+            g.MenuScreen();
+            g.ViewControls();
+            Event evt;
+            Object what;
+            g.action(evt, what);
+            */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     //plays level 2
-    void test2(){
-
+    @Test
+    public void test2(){
+        try {
+            Main.main();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    static List<TestInput> inputs;
+    record TestInput() implements Serializable{
+        void check(){
+            //checks assertions
+        };
+    };
 }
