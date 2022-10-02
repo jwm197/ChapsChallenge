53333adde7f0f67b9565555154ecc2f985215098
yuris702 <16365@wc.school.nz>
2022-10-02 14:02:25 +1300

    Updated makeLevel parameter

--- src/nz/ac/vuw/ecs/swen225/gp22/Domain/Level.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Domain/Level.java
@@ -3,7 +3,7 @@ package nz.ac.vuw.ecs.swen225.gp22.Domain;
 import java.util.List;
 
 public record Level(Model model) {
-    public static Level makeLevel(Player player, List<Entity> entities, List<Key> keys, List<Treasure> treasure, Tiles tiles, Runnable next, Runnable first) {
+    public static Level makeLevel(Player player, List<Entity> entities, List<Key> keys, List<Treasure> treasure, Tiles tiles, Runnable next, Runnable gameOver) {
         return new Level(new Model() {
             private Animator animator;
 
@@ -12,7 +12,7 @@ public record Level(Model model) {
             public List<Key> keys() { return keys; }
             public List<Treasure> treasure() { return treasure; }
             public Tiles tiles() { return tiles; }
-            public void onGameOver() { first.run(); }
+            public void onGameOver() { gameOver.run(); }
             public void onNextLevel() { next.run(); }
             public void bindAnimator(Animator a) { animator = a; }
             public Animator animator() { return animator; }
