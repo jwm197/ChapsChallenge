32dd3d7ab4c51dc948a014379704a9208e8bfb56
Anfri <haywarmatt@ecs.vuw.ac.nz>
2022-10-07 13:49:35 +1300

    Remove domain testing code

--- src/nz/ac/vuw/ecs/swen225/gp22/Renderer/DomainTesting/Tile.java
+++
@@ -1,6 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp22.Renderer.DomainTesting;
-
-import nz.ac.vuw.ecs.swen225.gp22.Renderer.TextureHandling.LayeredTexture;
-
-@Deprecated
-public record Tile(Position<Integer> pos, LayeredTexture texture) {}
