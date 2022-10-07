637cc72afceaa4a757b86c39e3d848457e308e33
jwm197 <jacob.mcbride@hotmail.co.nz>
2022-10-07 15:11:17 +1300

    added xml improvements

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/SaveRecordedGame.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/SaveRecordedGame.java
@@ -4,15 +4,28 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public record SaveRecordedGame(RecordedLevel level) {
-    public void saveXML(String path, String levelName) throws ParserException, IOException, DocumentException {
+    public void saveXML(String path, String fileName) throws ParserException, IOException, DocumentException {
         try {
-            saveLevel(new Persistency().createDoc(path, levelName));
+            OutputFormat format = OutputFormat.createPrettyPrint();
+            System.out.println("doesn't");
+            Document doc = saveLevel(new Persistency().createDoc(path, fileName));
+            System.out.println("makesdoc");
+            FileOutputStream fos = new FileOutputStream(path+fileName);
+            XMLWriter writer = new XMLWriter(fos, format);
+            writer.write(doc);
+            System.out.println("Save complete");
+
+
         } catch (ParserException | NullPointerException e) {
             throw new ParserException(e.getMessage());
         } catch (IOException e) {
@@ -25,13 +38,18 @@ public record SaveRecordedGame(RecordedLevel level) {
      * @param doc the document containing the level
      * @return the recorded level object containing the level name and the list of moves
      */
-    private void saveLevel(Document doc){
+    private Document saveLevel(Document doc){
+        System.out.println("starts to save");
         if (doc==null){
             throw new ParserException("document not found");
         }
-        Element level= doc.getRootElement().element("level").addAttribute("name", level().levelName());
+        System.out.println("starts to save");
+        Element level= doc.getRootElement().element("level");
+        level.addAttribute("name", level().levelName());
         level.setText(String.valueOf(level().levelName()));
         saveMoves(level);
+        System.out.println("saveslevel");
+        return doc;
 
 
     }
