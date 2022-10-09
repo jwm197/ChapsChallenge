cfe6bf064b23c02fa7295b4dbeeaeb40670482b4
Hanning Qu <quhann@ecs.vuw.ac.nz>
2022-10-10 01:47:19 +1300

    Update FuzzTest.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/Fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/Fuzz/FuzzTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 import java.util.stream.IntStream;
 
 
@@ -23,32 +24,39 @@ public class FuzzTest{
         }
     }
     //plays level 2
-    @Test
+    /*@Test
     public void test2(){
         try {
             inputs2.forEach(i->i.check());
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
     //inputs to be tested
     
-    static List<TestInput> inputs1 = List.of(new TestInput(Move.Load1, randomMoves(1000)));
-    static List<TestInput> inputs2 = List.of(new TestInput(Move.Load2, randomMoves(1000))); 
+    static List<TestInput> inputs1 = List.of(new TestInput(Move.Down, randomMoves(5)));
+    //static List<TestInput> inputs2 = List.of(new TestInput(Move.Down, randomMoves(1000))); 
     record TestInput(Move level, List<Move> moves) implements Serializable{//a collection of inputs
         void check(){
             Game g = new FuzzTest().new Game();
             ChapsChallenge c = new ChapsChallenge();
             c.menuScreen();
-            System.out.println(level);
+            c.gameScreen("level1.xml");
+            Stack<Move> stackOfMoves = new Stack<>();
+            stackOfMoves.addAll(moves);
+            c.afterMove = ()->{ if (performNextMove(g,c,stackOfMoves).isEmpty()) {c.afterMove=()->{};}};
             g.doMove(level, c);
-            for (var m:moves){
-                g.doMove(m, c);
-                
-            }
-            g.doMove(Move.Exit, c);
+           
+        }
+           // g.doMove(Move.Exit, c);
         };
-    };
+    
+    public static Stack<Move> performNextMove(Game g, ChapsChallenge c, Stack<Move> s) {
+    	if (s.isEmpty()) {return s;} 
+    	g.doMove(s.pop(), c);
+    	return s;
+    }
+    
     //game object for test
     public class Game{
         void doMove(Move move, ChapsChallenge g){//moves player accordingly   
@@ -61,7 +69,7 @@ public class FuzzTest{
                 g.performAction("UP");
                 case 3:
                 g.performAction("DOWN");
-                case 4:
+               /* case 4:
                 g.performAction("ESC");
                 case 5:
                 g.performAction("CTRL-1");
@@ -72,16 +80,16 @@ public class FuzzTest{
                 case 8:
                 g.performAction("CTRl-S");
                 case 9:
-                g.performAction("CTRL-X");
+                g.performAction("CTRL-X");*/
 
             }
         }
     }
     interface Input{}
-    enum Move implements Input {Left, Right, Up, Down, Pause, Load1, Load2, Load, Exit, Menu}// player moves possible
+    enum Move implements Input {Left, Right, Up, Down,}//Pause, Load1, Load2, Load, Exit, Menu}// player moves possible
     static List<Move> randomMoves(int size){//generates random moves
         return IntStream.range(0, size)
-        .map(i->new Random().nextInt(3))
+        .map(i->new Random().nextInt(Move.values().length))
         .mapToObj(ei->Move.values()[ei])
         .toList();
     }
