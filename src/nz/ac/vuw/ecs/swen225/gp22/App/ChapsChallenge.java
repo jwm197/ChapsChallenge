f64759cfa85e9399bbbf2c4dd840870934708204
Yuri Sidorov <sidoroyuri@ecs.vuw.ac.nz>
2022-10-13 12:50:53 +1300

    Revert "Merge branch 'main' into 'Domain'"
    This reverts merge request !118

--- src/nz/ac/vuw/ecs/swen225/gp22/App/ChapsChallenge.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/App/ChapsChallenge.java
@@ -969,7 +969,6 @@ public class ChapsChallenge extends JFrame{
 		catch (ParserException e) {e.printStackTrace(); }
 		catch (IOException e) {e.printStackTrace(); }
 		catch (DocumentException e) {e.printStackTrace();}
-		if (l == null) return new boolean[0][0];
 		Tiles t = l.model().tiles();
 		return new boolean[t.height()][t.width()];
 	}
